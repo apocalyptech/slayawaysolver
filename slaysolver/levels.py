5f902ee61a81d928e0c16882c4d1fd6549db9b05
CJ Kucera <pez@apocalyptech.com>
2016-11-08 16:55:57 -0600

    All of the main SC5 levels are here now

--- slaysolver/levels.py
+++ slaysolver/levels.py
@@ -3452,6 +3452,375 @@ class Levels(object):
         return level
 
     @staticmethod
+    def s5_s05():
+
+        level = Level('Slayaway Camp 5, Scene 5 - Boom Beach', 8, 7,
+            7, 3,
+            7, 0)
+
+        level.wall_box(1, 4)
+        level.wall_box(1, 5)
+        level.wall_box(4, 6)
+        level.wall_box(7, 6)
+
+        level.short_wall_south(0, 2)
+        level.short_wall_south(3, 0)
+        level.short_wall_south(5, 0)
+        level.short_wall_west(3, 2)
+        level.short_wall_east(3, 2)
+        level.short_wall_east(4, 2)
+
+        level.short_wall_west(1, 1)
+        level.short_wall_south(1, 1)
+        level.short_wall_west(2, 2)
+        level.short_wall_west(2, 3)
+        level.short_wall_south(2, 3)
+        level.short_wall_south(3, 3)
+        level.short_wall_south(4, 3)
+        level.short_wall_south(5, 3)
+        level.short_wall_east(5, 3)
+        level.short_wall_east(5, 2)
+        level.short_wall_east(5, 1)
+        level.short_wall_north(6, 1)
+        level.short_wall_north(7, 1)
+
+        level.set_mine(2, 0)
+        level.set_mine(4, 0)
+        level.set_mine(2, 2)
+
+        level.add_victim(2, 3)
+        level.add_victim(5, 3)
+        level.add_victim(5, 5)
+
+        return level
+
+
+        return level
+
+    @staticmethod
+    def s5_s06():
+
+        level = Level('Slayaway Camp 5, Scene 6 - Sand Castle Scarin\'', 6, 6,
+            1, 5,
+            4, 3)
+
+        level.wall_box(1, 1)
+        level.wall_box(5, 0)
+        level.wall_box(0, 3)
+
+        level.wall_west(3, 3)
+        level.wall_west(3, 5)
+        level.wall_north(4, 4)
+        level.wall_east(4, 4)
+
+        level.escape_north(3)
+        level.escape_west(0)
+
+        level.add_victim(1, 0)
+        level.add_victim(3, 1)
+        level.add_victim(3, 2)
+
+        return level
+
+    @staticmethod
+    def s5_s07():
+
+        level = Level('Slayaway Camp 5, Scene 7 - Deja Vu', 7, 7,
+            5, 5,
+            6, 0)
+
+        level.short_wall_east(0, 5)
+        level.short_wall_east(3, 6)
+
+        level.short_wall_south(2, 3)
+        level.short_wall_south(3, 3)
+        level.short_wall_south(4, 3)
+        level.short_wall_south(6, 3)
+
+        level.short_wall_west(6, 0)
+        level.short_wall_west(6, 1)
+        level.short_wall_west(6, 2)
+
+        level.short_wall_west(1, 3)
+        level.short_wall_west(1, 2)
+        level.short_wall_west(1, 1)
+        level.short_wall_north(1, 1)
+        level.short_wall_north(2, 1)
+        level.short_wall_east(2, 1)
+        level.short_wall_south(2, 1)
+        level.short_wall_north(3, 1)
+        level.short_wall_south(3, 1)
+        level.short_wall_north(4, 1)
+        level.short_wall_east(4, 1)
+        level.short_wall_east(4, 2)
+
+        level.set_mine(6, 1)
+
+        level.add_cabinet_ns(1, 4)
+        level.add_cabinet_ns(5, 3)
+
+        level.add_victim(1, 3)
+        level.add_victim(4, 3)
+
+        return level
+
+    @staticmethod
+    def s5_s08():
+
+        level = Level('Slayaway Camp 5, Scene 8 - Rearranger Dangers', 7, 7,
+            6, 6,
+            6, 2)
+
+        level.wall_south(0, 0)
+        level.wall_south(1, 0)
+        level.wall_south(2, 0)
+        level.wall_east(2, 0)
+
+        level.wall_west(4, 0)
+        level.wall_south(4, 0)
+        level.wall_west(5, 1)
+        level.wall_south(5, 1)
+        level.wall_south(6, 1)
+
+        level.wall_box(3, 6)
+
+        level.wall_south(1, 3)
+        level.wall_south(2, 3)
+        level.wall_west(6, 4)
+        level.wall_west(6, 5)
+
+        level.escape_north(3)
+        level.escape_west(3)
+
+        level.add_victim(2, 3)
+        level.add_victim(3, 3)
+        level.add_victim(3, 4)
+        level.add_victim(4, 6)
+
+        return level
+
+    @staticmethod
+    def s5_s09():
+
+        level = Level('Slayaway Camp 5, Scene 9 - Cop Call Confusion', 7, 7,
+            0, 2,
+            5, 4)
+
+        level.wall_box(0, 1)
+
+        level.wall_north(0, 4)
+        level.wall_east(0, 4)
+        level.wall_east(0, 5)
+        level.wall_north(1, 6)
+        level.wall_east(1, 6)
+
+        level.wall_west(4, 6)
+        level.wall_north(4, 6)
+        level.wall_north(5, 6)
+        level.wall_north(6, 6)
+
+        level.wall_west(4, 3)
+        level.wall_north(4, 3)
+        level.wall_south(4, 3)
+        level.wall_north(5, 3)
+        level.wall_south(5, 3)
+        level.wall_east(5, 3)
+
+        level.wall_west(2, 4)
+        level.wall_south(2, 4)
+
+        level.wall_south(2, 0)
+        level.wall_south(3, 0)
+        level.wall_south(5, 4)
+
+        level.add_phone_pair(0, 0, 6, 5)
+        level.add_phone_pair(2, 4, 3, 1)
+
+        level.add_swat(4, 2, DIR_N)
+        level.add_swat(4, 4, DIR_E)
+        level.add_cop(1, 0, DIR_S)
+        level.add_cop(2, 0, DIR_S)
+
+        level.add_victim(5, 0)
+        level.add_victim(6, 1)
+        level.add_victim(3, 2)
+
+        return level
+
+    @staticmethod
+    def s5_s10():
+
+        level = Level('Slayaway Camp 5, Scene 10 - Nighttime on Fridge Beach', 7, 6,
+            1, 4,
+            3, 5,
+            14)
+
+        level.wall_box(2, 3)
+
+        level.wall_north(4, 3)
+        level.wall_west(4, 3)
+        level.wall_east(4, 3)
+        level.wall_west(4, 4)
+        level.wall_east(4, 4)
+        level.wall_south(4, 4)
+
+        level.wall_south(5, 1)
+        level.wall_south(1, 4)
+
+        level.escape_north(3)
+
+        level.add_cabinet_we(5, 0)
+        level.add_cabinet_ns(0, 3)
+
+        level.add_victim(1, 2)
+        level.add_victim(3, 3)
+
+        return level
+
+    @staticmethod
+    def s5_s11():
+
+        level = Level('Slayaway Camp 5, Scene 11 - Cabana Carnage', 8, 7,
+            1, 3,
+            1, 1)
+
+        level.wall_west(4, 0)
+        level.wall_west(4, 1)
+        level.wall_south(4, 1)
+        level.wall_south(5, 1)
+        level.wall_east(5, 1)
+        level.wall_south(6, 0)
+        level.wall_east(6, 0)
+
+        level.wall_west(6, 6)
+        level.wall_north(6, 6)
+        level.wall_north(7, 6)
+
+        level.wall_south(1, 0)
+        level.wall_south(0, 2)
+        level.wall_east(1, 3)
+        level.wall_east(2, 6)
+
+        level.wall_west(7, 2)
+        level.wall_west(7, 3)
+        level.wall_west(7, 4)
+
+        level.escape_north(2)
+        level.escape_west(5)
+
+        level.set_mine(2, 6)
+
+        level.add_swat(5, 4, DIR_W)
+        level.add_swat(3, 6, DIR_N)
+
+        level.add_victim(2, 1)
+        level.add_victim(2, 5)
+        level.add_victim(1, 6)
+        level.add_victim(5, 5)
+
+        return level
+
+    @staticmethod
+    def s5_s12():
+
+        level = Level('Slayaway Camp 5, Scene 12 - Sandy Sophomore Sacrifices', 7, 7,
+            1, 2,
+            0, 1)
+
+        level.wall_west(5, 5)
+        level.wall_north(5, 5)
+        level.wall_south(5, 5)
+        level.wall_north(6, 5)
+        level.wall_south(6, 5)
+
+        level.wall_north(1, 1)
+        level.wall_east(1, 1)
+        level.wall_north(2, 1)
+        level.wall_north(3, 1)
+        level.wall_east(3, 1)
+        level.wall_south(4, 1)
+
+        level.wall_east(0, 2)
+        level.wall_east(0, 3)
+
+        level.wall_south(2, 2)
+        level.wall_east(2, 2)
+
+        level.wall_west(3, 4)
+        level.wall_north(3, 4)
+
+        level.wall_north(5, 3)
+        level.wall_east(5, 3)
+
+        level.set_mine(1, 6)
+        level.set_mine(5, 6)
+
+        level.add_victim(2, 3)
+        level.add_victim(3, 5)
+        level.add_victim(0, 6)
+        level.add_victim(6, 6)
+
+        return level
+
+    @staticmethod
+    def s5_s13():
+
+        # Not sure at the moment where the exit is
+
+        level = Level('Slayaway Camp 5, Scene 13 - Sharks and Lasers', 9, 9,
+            3, 8,
+            3, 7)
+
+        level.wall_box(0, 0)
+        level.wall_box(0, 8)
+
+        level.wall_north(8, 7)
+        level.wall_west(8, 7)
+        level.wall_west(8, 8)
+
+        level.wall_west(4, 7)
+        level.wall_north(4, 7)
+        level.wall_south(4, 7)
+        level.wall_north(5, 7)
+        level.wall_south(5, 7)
+        level.wall_east(5, 7)
+
+        level.wall_north(6, 6)
+        level.short_wall_west(6, 6)
+
+        level.short_wall_south(1, 2)
+        level.short_wall_east(1, 2)
+
+        level.short_wall_west(4, 1)
+        level.short_wall_south(4, 1)
+        level.short_wall_south(5, 1)
+        level.short_wall_east(5, 2)
+
+        level.short_wall_north(7, 1)
+        level.short_wall_west(7, 1)
+        level.short_wall_west(7, 2)
+        level.short_wall_west(7, 3)
+
+        level.set_hazard(3, 4)
+        level.set_hazard(4, 4)
+        level.set_hazard(3, 5)
+        level.set_hazard(4, 5)
+
+        level.add_cabinet_ns(8, 2)
+        level.add_cabinet_ns(7, 3)
+
+        level.add_swat(8, 1, DIR_W)
+        level.add_swat(2, 4, DIR_E)
+        level.add_swat(2, 6, DIR_E)
+        level.add_cop(6, 0, DIR_S)
+
+        level.add_victim(4, 1)
+        level.add_victim(2, 2)
+        level.add_victim(3, 6)
+
+        return level
+
+    @staticmethod
     def s6_d4():
 
         level = Level('Slayaway Camp 6, Deleted Scene 4', 9, 9,
--- tests.py
+++ tests.py
@@ -1969,6 +1969,274 @@ class LevelTests(unittest.TestCase):
             DIR_S,
         ])
 
+    def test_s5_s05(self):
+        game = Game(levels.get_level('s5_s05'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_E,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_E,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+        ])
+
+    def test_s5_s06(self):
+        game = Game(levels.get_level('s5_s06'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_S,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+        ])
+
+    def test_s5_s07(self):
+        game = Game(levels.get_level('s5_s07'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_S,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_W,
+            DIR_S,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+        ])
+
+    def test_s5_s08(self):
+        game = Game(levels.get_level('s5_s08'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_E,
+            DIR_N,
+            DIR_E,
+        ])
+
+    def test_s5_s09(self):
+        game = Game(levels.get_level('s5_s09'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_E,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_W,
+            DIR_N,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_S,
+            DIR_W,
+        ])
+
+    def test_s5_s10(self):
+        game = Game(levels.get_level('s5_s10'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_N,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_S,
+        ])
+
+    def test_s5_s11(self):
+        game = Game(levels.get_level('s5_s11'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_W,
+            DIR_S,
+            DIR_N,
+            DIR_E,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_N,
+        ])
+
+    def test_s5_s12(self):
+        game = Game(levels.get_level('s5_s12'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_W,
+            DIR_N,
+        ])
+
+    def test_s5_s13(self):
+        game = Game(levels.get_level('s5_s13'))
+        game.solve()
+        self.assertEqual(game.solution, [
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_W,
+            DIR_N,
+            DIR_E,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+            DIR_S,
+            DIR_E,
+            DIR_N,
+            DIR_E,
+            DIR_S,
+            DIR_W,
+            DIR_S,
+            DIR_W,
+            DIR_N,
+            DIR_W,
+            DIR_S,
+            DIR_E,
+        ])
+
     def test_s6_d4(self):
         game = Game(levels.get_level('s6_d4'))
         game.solve()
